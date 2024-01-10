685be9f14ede4ea7b9ffc9ea295220b5389afa0c
Akeem Amusat <aoamusat@gmail.com>
2024-01-10 14:46:33 +0100

    Switch python runtime to 3.11 & Updated build.sh

--- packages/qrpkg/qrcode/__main__.py
+++ packages/qrpkg/qrcode/__main__.py
@@ -1,27 +1,19 @@
 import qrcode
 import base64
 import io
-from typing import Any
+from typing import Any, Dict
 
-def main(data:Any)->str:
-    """
-    Generates a QR code image based on the provided data and returns it as a base64-encoded data URI.
 
-    Args:
-    - data (Any): The input data to be encoded into the QR code.
-
-    Returns:
-    - str: A base64-encoded data URI representing the generated QR code image.
-
-    The function generates a QR code using the provided data using the qrcode library.
-    It converts the generated QR code image to a base64-encoded data URI and returns it.
-    """
+def main(event, context) -> Dict[str, Any]:
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_H,
         box_size=10,
         border=1,
     )
+    data = event.get("data", None)
+    if data is None:
+        data = ""
     qr.add_data(data)
     qr.make(fit=True)
 
@@ -29,8 +21,12 @@ def main(data:Any)->str:
 
     # Convert image to base64-encoded data URI
     buffered = io.BytesIO()
-    img.save(buffered, format="PNG")
+    img.save(buffered)
     img_str = base64.b64encode(buffered.getvalue()).decode()
     data_uri = f"data:image/png;base64,{img_str}"
 
-    return data_uri
+    return {
+        "body": {
+            "qrcode": data_uri,
+        }
+    }
